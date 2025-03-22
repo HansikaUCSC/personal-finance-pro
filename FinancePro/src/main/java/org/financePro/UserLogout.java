0bf76182dc375fe4296494bb6688eb965c13f458
Hansika Rajapaksha <hansikarajapaksha987@gmail.com>
2025-03-22 13:36:14 +0530

    modify the logout class with objects accessing through property file

--- FinancePro/src/main/java/org/financePro/UserLogout.java
+++ FinancePro/src/main/java/org/financePro/UserLogout.java
@@ -1,17 +1,21 @@
 package org.financePro;
 
 import org.financePro.utils.GlobalConfigurations;
+import org.financePro.utils.PropertyFileReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class UserLogout {
     WebDriver driver = GlobalConfigurations.getInstance().getDriver();
+    PropertyFileReader prop = new PropertyFileReader();
+    String headerDropdown = prop.getProperty("globalObjects","header.dropdown.element");
+    String logoutButton = prop.getProperty("globalObjects","logout.dropdown.option.element");
 
     public void logOut(){
-        GlobalConfigurations.getInstance().waitUntilNextElementAppears(By.id("btn_dropDownHeader"),30);
-        driver.findElement(By.id("btn_dropDownHeader")).click();
-        GlobalConfigurations.getInstance().waitUntilNextElementAppears(By.id("btn_logout"),30);
-        driver.findElement(By.id("btn_logout")).click();
+        GlobalConfigurations.getInstance().waitUntilNextElementAppears(By.id(headerDropdown),30);
+        driver.findElement(By.id(headerDropdown)).click();
+        GlobalConfigurations.getInstance().waitUntilNextElementAppears(By.id(logoutButton),30);
+        driver.findElement(By.id(logoutButton)).click();
     }
     public void verifyLogout(String expectedURL){
         String currentURL = driver.getCurrentUrl();
